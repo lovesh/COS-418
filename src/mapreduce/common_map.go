bf8bbf259882f8e2d63907a6220e526fbcdc53e9
Lovesh <lovesh.harchandani@evernym.com>
2017-01-29 20:34:28 +0530

    assignment 1 done

---
+++ src/mapreduce/common_map.go
@@ -0,0 +1,80 @@
+package mapreduce
+
+import (
+	"hash/fnv"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+)
+
+// doMap does the job of a map worker: it reads one of the input files
+// (inFile), calls the user-defined map function (mapF) for that file's
+// contents, and partitions the output into nReduce intermediate files.
+
+var hashes = make(map[string]int)
+
+func doMap(
+	jobName string, // the name of the MapReduce job
+	mapTaskNumber int, // which map task this is
+	inFile string,
+	nReduce int, // the number of reduce task that will be run ("R" in the paper)
+	mapF func(file string, contents string) []KeyValue,
+) {
+	// TODO:
+	// You will need to write this function.
+	// You can find the filename for this map task's input to reduce task number
+	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
+	// below doMap) should be used to decide which file a given key belongs into.
+	//
+	// The intermediate output of a map task is stored in the file
+	// system as multiple files whose name indicates which map task produced
+	// them, as well as which reduce task they are for. Coming up with a
+	// scheme for how to store the key/value pairs on disk can be tricky,
+	// especially when taking into account that both keys and values could
+	// contain newlines, quotes, and any other character you can think of.
+	//
+	// One format often used for serializing data to a byte stream that the
+	// other end can correctly reconstruct is JSON. You are not required to
+	// use JSON, but as the output of the reduce tasks *must* be JSON,
+	// familiarizing yourself with it here may prove useful. You can write
+	// out a data structure as a JSON string to a file using the commented
+	// code below. The corresponding decoding functions can be found in
+	// common_reduce.go.
+	//
+	//   enc := json.NewEncoder(file)
+	//   for _, kv := ... {
+	//     err := enc.Encode(&kv)
+	//
+	// Remember to close the file after you have written all the values!
+
+	dat, err := ioutil.ReadFile(inFile)
+	if err != nil {
+		fmt.Println("Error opening file ", inFile)
+	} else {
+		kvs := mapF(inFile, string(dat))
+		// Assuming each key in []KeyValue is unique
+		partitions := make([][]KeyValue, nReduce)
+
+		for i := range kvs {
+			k := kvs[i].Key
+			r, ok := hashes[k]
+			if !ok {
+				r = int(ihash(k)) % nReduce
+				hashes[k] = r
+			}
+			partitions[r] = append(partitions[r], kvs[i])
+
+		}
+
+		for i := range partitions {
+			j, _ := json.Marshal(partitions[i])
+			ioutil.WriteFile(reduceName(jobName, mapTaskNumber, i), []byte(j), 0644)
+		}
+	}
+}
+
+func ihash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
---
+++ src/mapreduce/common_reduce.go
@@ -0,0 +1,73 @@
+package mapreduce
+
+import (
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+	"os"
+)
+
+// doReduce does the job of a reduce worker: it reads the intermediate
+// key/value pairs (produced by the map phase) for this task, sorts the
+// intermediate key/value pairs by key, calls the user-defined reduce function
+// (reduceF) for each key, and writes the output to disk.
+func doReduce(
+	jobName string, // the name of the whole MapReduce job
+	reduceTaskNumber int, // which reduce task this is
+	nMap int, // the number of map tasks that were run ("M" in the paper)
+	reduceF func(key string, values []string) string,
+) {
+	// TODO:
+	// You will need to write this function.
+	// You can find the intermediate file for this reduce task from map task number
+	// m using reduceName(jobName, m, reduceTaskNumber).
+	// Remember that you've encoded the values in the intermediate files, so you
+	// will need to decode them. If you chose to use JSON, you can read out
+	// multiple decoded values by creating a decoder, and then repeatedly calling
+	// .Decode() on it until Decode() returns an error.
+	//
+	// You should write the reduced output in as JSON encoded KeyValue
+	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
+	// you to use JSON here because that is what the merger then combines the
+	// output from all the reduce tasks expects. There is nothing "special" about
+	// JSON -- it is just the marshalling format we chose to use. It will look
+	// something like this:
+	//
+	// enc := json.NewEncoder(mergeFile)
+	// for key in ... {
+	// 	enc.Encode(KeyValue{key, reduceF(...)})
+	// }
+	// file.Close()
+
+	kvs := make(map[string] []string)
+	for m := 0; m < nMap; m++ {
+		fileName := reduceName(jobName, m, reduceTaskNumber)
+		dat, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			fmt.Println("Error opening file ", fileName)
+		} else {
+			var items []KeyValue
+			json.Unmarshal(dat, &items)
+			for _, item := range items {
+				k := item.Key
+				v := item.Value
+				vals, ok := kvs[k]
+				if !ok {
+					vals = []string{}
+					kvs[k] = vals
+				}
+				kvs[k] = append(kvs[k], v)
+			}
+		}
+	}
+	mergeFileName := mergeName(jobName, reduceTaskNumber)
+	file, err := os.Create(mergeFileName)
+	if err != nil {
+		fmt.Println("Cannot open file ", mergeFileName)
+	}
+	enc := json.NewEncoder(file)
+	for key, val := range kvs {
+		enc.Encode(KeyValue{key, reduceF(key, val)})
+	}
+	file.Close()
+}
